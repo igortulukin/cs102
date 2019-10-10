5b05a24b3ca8e45b8aeec29ca6d25e809febd43b
Игорь Тюлюкин <igor-tulukin@mail.ru>
2019-10-10 17:28:39 +0300

    RSA done

--- homework01/RSA.py
+++ homework01/RSA.py
@@ -1,9 +1,13 @@
+import random
+
+
 def is_prime(n: int) -> bool:
-    for i in range(2,n):
+    for i in range(2, n):
         if n % i == 0:
             return False
         else:
             return True
+
 
 def gcd(a: int, b: int) -> int:
     while (a % b != 0):
@@ -12,26 +16,37 @@ def gcd(a: int, b: int) -> int:
         b = c
     return b 
 
-def generate_keypair(p: int, q: int) -> Tuple[Tuple[int, int], Tuple[int, int]]:
+
+def multiplicative_inverse(e: int, phi: int) -> int:
+    div = []
+    rows = 0
+    phi_temp = phi
+    while phi_temp % e != 0:
+        div.append(phi_temp // e)
+        phi_temp, e = e, phi_temp % e
+        rows += 1
+    x = 0
+    y = 1
+    for i in range(rows - 1, -1, -1):
+        x, y = y, x - (y * div[i])
+    return y % phi
+
+
+def generate_keypair(p: int, q: int) -> ((int, int), (int, int)):
+
     if not (is_prime(p) and is_prime(q)):
         raise ValueError('Both numbers must be prime.')
     elif p == q:
         raise ValueError('p and q cannot be equal')
-    n = p*q
-    phi = (p-1)*(q-1)
+
+    n = p * q
+    phi = (p - 1) * (q - 1)
+
     e = random.randrange(1, phi)
     g = gcd(e, phi)
     while g != 1:
         e = random.randrange(1, phi)
         g = gcd(e, phi)
 
-    # Use Extended Euclid's Algorithm to generate the private key
     d = multiplicative_inverse(e, phi)
-    # Return public and private keypair
-    # Public key is (e, n) and private key is (d, n)
-    return ((e, n), (d, n))
-
-
-p = int(input("Enter first number"))
-q = int(input("Enter second number"))
-print(is_prime(p))
+    return (e, n), (d, n)
