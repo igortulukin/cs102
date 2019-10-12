f870462feacb5ca4f78a80e24e7a6730b16091e7
Игорь Тюлюкин <igor-tulukin@mail.ru>
2019-10-12 10:33:50 +0300

    RSA done

--- homework01/RSA.py
+++ homework01/RSA.py
@@ -2,14 +2,10 @@ import random
 
 
 def is_prime(n: int) -> bool:
-    if n == 2 :
-        return True
-    else :
-        for i in range(2, n):
-            if n % i == 0 :
-                return False
-            else:
-                return True
+    for i in range(2, n):
+        if n % i == 0 :
+            return False
+    return True
 
 
 def gcd(a: int, b: int) -> int:
